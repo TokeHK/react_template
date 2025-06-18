17311251ed3d050464194596341849fc0b029ebf
TokeHK <toha7@videndjurs.net>
2025-06-18 13:12:57 +0200

    basic navbar and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-white text-gray-800">
+    <div className="flex flex-col min-h-screen bg-white text-gray-800">
       <Navbar />
-      <main className="p-4">
+      <main className="flex-1 p-4">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </main>
+      <Footer />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-import './index.css'; // Tailwind config er i index.css
+import './App.css'; // Tailwind config er i App.css
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
